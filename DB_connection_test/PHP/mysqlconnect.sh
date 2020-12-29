51617723a122bbfe306317db905f7445e0874366
renhanli <renhanli@gmail.com>
2020-12-29 12:36:08 +0800

    Delete mysqlconnect.sh

--- DB_connection_test/PHP/mysqlconnect.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Ask the user for login details
-read -p 'MySQL server: ' server
-read -p 'Username: ' user
-read -sp 'Password: ' passwd
-read -p 'Database: ' database
-
-echo "Run connection test: "
-php ./mysqlconnect.php $server $user $passwd $database
